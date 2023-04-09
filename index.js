9df8cef1838e755c51c463d54d31ccd889ec4b74
Thanakorn Boonyasena <Thanakorn.Boonyasena@gmail.com>
2023-04-09 08:54:13 +0700

    updata 0.2

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.urlencoded ({extended: true}));
-app.use(express.static(path.join(__dirname,"pages")))
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+  });
+
+  app.get('/lv1', (req, res) => {
+    const password = {};
+    res.render('lv1', { password });
+  });
+  
+  
 
 
-app.post('/level1-send', (req,res) => {
+  app.post('/level1-send', (req, res) => {
     const password = req.body;
-    const passwordlist = Object.values(password);
-    console.log(passwordlist);
-})
-
-app.listen(8080 ,() => console.log("Server is running"));   
+    res.render('lv1',{password});
+  });
+  
+app.listen(8080 ,() => console.log("Server is running"))
